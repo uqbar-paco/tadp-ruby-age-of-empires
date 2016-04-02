07304b352e1517b1afb0217bd17a78b07265704d
Axel Bergh <abergh@despegar.com>
2016-04-01 21:50:23 -0300

    terminado mixins con alias y modelado bloques

--- src/age.rb
+++ src/age.rb
@@ -43,12 +43,45 @@ end
 
 class Guerrero
   include Atacante
+  alias_method :descansar_atacante, :descansar
   include Defensor
+  alias_method :descansar_defensor, :descansar
+
+  attr_accessor :peloton
 
   def initialize(potencial_ofensivo=20, energia=100, potencial_defensivo=10)
     self.potencial_ofensivo = potencial_ofensivo
     self.energia = energia
     self.potencial_defensivo = potencial_defensivo
+  end
+
+  def descansar
+    self.descansar_atacante
+    self.descansar_defensor
+  end
+
+  def sufri_danio(danio)
+    super(danio)
+    if (!peloton.nil?)
+      self.peloton.lastimado
+    end
+  end
+
+end
+
+class Kamikaze
+  include Defensor
+  include Atacante
+
+  def initialize
+    @potencial_ofensivo = 250
+    @energia = 100
+    @potencial_defensivo = 10
+  end
+
+  def atacar(defensor)
+    super(defensor)
+    @energia = 0
   end
 end
 
@@ -95,4 +128,55 @@ class Muralla
     self.energia = energia
   end
 
+end
+
+class Peloton
+  attr_accessor :guerreros, :estrategia, :retirado
+
+  def self.cobarde integrantes
+    self.new(integrantes,
+             proc {|peloton| peloton.retirate})
+  end
+
+  def initialize(integrantes, estrategia)
+    self.guerreros =integrantes
+    integrantes.each { |i| i.peloton = self }
+    self.estrategia = estrategia
+    self.retirado = false
+  end
+
+  def descansar
+    cansados = self.guerreros.select {|integrante|
+      ! integrante.esta_descansado?
+    }
+    cansados.each do |integrante|
+      integrante.descansar
+    end
+  end
+
+  def lastimado
+    estrategia.call(self)
+  end
+
+  def retirate
+    @retirado = true
+  end
+end
+
+class Descansador
+  def call(peloton)
+    peloton.descansar
+  end
+end
+
+class Cobarde
+  def call(peloton)
+    peloton.retirate
+  end
+end
+
+class Masoquista
+  def call(peloton)
+    peloton.retirado = false
+  end
 end
